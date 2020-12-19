238eecc9da4b383c60e6016956da823851ff88b4
Koli <sokol.muslija@gmail.com>
2020-12-19 19:36:29 +0100

    implementati messagi scambio carta

--- screens/TradeScreen.js
+++ screens/TradeScreen.js
@@ -1,22 +1,103 @@
-import React from 'react'
-import {View, Text, Pressable} from 'react-native'
+import React, {useEffect, useState, useContext} from 'react'
+import {View, Text} from 'react-native'
 import Button from '../components/Button'
-import CardScreen from '../screens/CardScreen'
+import {AuthContext} from '../context/AuthContext'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import Alert from '../components/Alert'
 
-export default function TradeScreen(props) {
+export default function TradeScreen(props, {navigate}) {
     const nav = props.navigation
-    const id = props.route.params.id
+    const card_id = props.route.params.id
+
+    const { token } = useContext(AuthContext)
+    
+    const [error, setError] = useState(false)
+    const [alertMessage, setAlertMessage] = useState('')
+    const [messageOpen, setMessageOpen] = useState(false)
+    const [hasPermission, setHasPermission] = useState(null)
+    const [scanned, setScanned] = useState(false)
+    const [readQR, setReadQR] = useState(false)
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Il codice è stato salvato , premi invia per inviare la carta`);
+    }
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    const qrScan = () => {
+        setReadQR(true)
+    }
+
+    const tradeCard = () => {
+        fetch(`https://tree-rn-server.herokuapp.com/move-card`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token,
+            },
+            body: JSON.stringify({
+                "card_id": card_id,
+                "portfolio_code": portofolio_code
+            }),
+            method: 'POST'
+        })
+            .then(response => response.json())
+            .then(response => {
+
+                if (response.result == true) {
+                    setMessageOpen(true)
+                    setError(false)
+                    setAlertMessage('Il trasferimento è avvenuto con successo')
+                }
+                if (response.result == false) {
+                    setMessageOpen(true)
+                    setError('danger')
+                    setAlertMessage(resp.errors[0].message)
+                }
+
+            })
+    }
+
+    const backToList = () => {
+        props.navigation.navigate('CollectionScreen')
+    }
     
     return (
-        <View>
-            <Text>{props.route.params.name}</Text>
-            <Text>{props.route.params.game}</Text>
-            <Text>{props.route.params.id}</Text>
-            <Button>Trade Card</Button>
-            <Pressable onPress={() => nav.navigate('TradeScreenB', props.route.params)}>
-                <Text>No QR Code?</Text>
-            </Pressable>
+        <>
+  <Alert open={messageOpen} message={alertMessage} onClose={() => { error ? setMessageOpen() : backToList() }} typology={error ? 'danger' : 'success'} /> 
+        <View >
+            <View style={{ justifyContent: 'center' }}>
+                
+                <Button onPress={() => qrScan()}>SCAN QR CODE</Button>
+
+                {
+                    (readQR) &&
+                    <BarCodeScanner
+                        style={{ height: 500, width: 400, }}
+                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+                        
+                    />
+                }
+
+                <Button onPress={() => tradeCard()}>SEND CARD</Button>
+                <Button onPress={()=> nav.navigate('TradeScreenB', props.route.params)}>NO QR CODE?</Button>
+            </View>
         </View>
+
+    </>
     )
 }
 
